8c3dbf1e97a6da534c7f90916f576ccbbe48e7d7
Martin Reece <martin@Martins-Air.home>
2022-05-23 19:06:05 -0400

    I made some changes to this JS file. I change the reset function to a reload function so when a user clicks on the playAgain button, it will reload the webpage

--- verb.js
+++ verb.js
@@ -1,7 +1,7 @@
 // Create a variable named "output" and grab the div element named "outputText" from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
 let output = document.getElementById('outputText');
 
-// Grab my from the DOM the span element "changeForward"
+// Grab my from the DOM the span element "guesses"
 let int = document.getElementById('guesses');
 
 // I'm generating a random value by creating a variable, named "number" and assigned Math.floor() with Math.random() times by 100. This will generate a random number that isn't 1 or a whole number and round it to the nearest integer 
@@ -21,7 +21,7 @@ function myFunction(e){
     numberOfGuesses += 1;
     guesses.innerHTML = numberOfGuesses;
 
-    if (input == number){
+    if (input === number){
         output.innerHTML = `You guessed right, your number was ${number}`
         console.log(input);
     } else if (input < number){
@@ -32,19 +32,13 @@ function myFunction(e){
     if (numberOfGuesses > 3) {
         guesses.innerHTML = `Sorry but all three guesses were wrong. Correct number is: ${number}`;
     }
+   
 }
 
 
-// RESET BUTTON
-// Create a function named "fun()" with no parameters that will reset this number guessing game 
-// Inside this function, I will select the id name of "form" from the DOM with the document.getElementById() and use the reset() property.
-// Select the id name of "userInput" from the DOM with the document.getElementById() and accessed the inner HTML content of that element and assigned it to an empty strings. This will actually reset the data.
-// Select the id name of "outputText" from the DOM with the document.getElementById() and accessed the inner HTML content of that element and assigned it to an empty strings. This will actually reset the data.
-
-function fun(){
-    document.getElementById('form').reset();
-
-    document.getElementById('userInput').innerHTML = "";
-    document.getElementById('outputText').innerHTML = "";
-    document.getElementById('guesses').innerHTML = "" + 0;
+// PlAY AGAIN BUTTON
+// Create a function named "reloadThePage()" with no parameters that will reload this number guessing game web browser
+function reloadThePage(){
+    window.location.reload();
+     
 }
